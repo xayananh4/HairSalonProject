6a56e773cc8cb9a4b0c1688d0d94769751fbf074
Viradeth Xay-Ananh <xayananh4@gmail.com>
2023-03-10 15:56:07 -0800

    take user to index cs page of clients controller

---
+++ HairSalon/Controllers/ClientsController.cs
@@ -0,0 +1,22 @@
+using Microsoft.AspNetCore.Mvc;
+using System.Collections.Generic;
+using HairSalon.Models;
+using System.Linq;
+
+namespace HairSalon.Controllers
+{
+  public class ClientsController : Controller
+  {
+    private readonly HairSalonContext _db;
+
+    public ClientsController(HairSalonContext db)
+    {
+      _db = db;
+    }
+    public ActionResult Index()
+    {
+      List<Client> model = _db.Clients.ToList();
+      return View(model);
+    }
+  }
+}
