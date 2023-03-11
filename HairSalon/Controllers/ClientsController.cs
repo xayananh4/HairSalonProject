5a44cd5f7b0a441fa7d3079eb056d1321517e06c
Viradeth Xay-Ananh <xayananh4@gmail.com>
2023-03-10 19:06:43 -0800

    update home page, update string: Welcome Stylists and Clients MVC App

--- HairSalon/Controllers/ClientsController.cs
+++ HairSalon/Controllers/ClientsController.cs
@@ -18,5 +18,17 @@ namespace HairSalon.Controllers
       List<Client> model = _db.Clients.ToList();
       return View(model);
     }
+    public ActionResult Create()
+    {
+      return View();
+    }
+
+    [HttpPost]
+    public ActionResult Create(Client client)
+    {
+      _db.Clients.Add(client);
+      _db.SaveChanges();
+      return RedirectToAction("Index");
+    }
   }
 }
